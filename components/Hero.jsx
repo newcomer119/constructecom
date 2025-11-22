8aded8d7a4266ac3eab75943e984061ec7981a08
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-11-22 17:50:01 +0530

    added assets to public for import

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <section>
-        Hero
+    <section className="h-[70vh] bg-hero bg-no-repeat bg-cover bg-center relative">
+      {/* overlay */}
+      <div className="absolute inset-0 bg-gradient-to-l from black/0 via-black/50 to-black/70 z-10"></div>
+      <div className="container mx-auto h-full flex items-center">
+        <div className="z-20 text-white">
+          <h1 className="h1 text-white mb-4">
+            <span className="text-accent">Building</span> robust lasting
+            solutions
+          </h1>
+          <p className="mb-9">
+            From Concept to completion, we ensure every details is optimized for
+            strength and endurance, creating solutions that inspire confidence
+            and stand firm for years
+          </p>
+          {/* btn  */}
+          <div>
+            <button>See Our Work</button>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
