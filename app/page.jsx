76bce675e34deac70d1f966ca6fe33beebbf1401
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-11-22 15:12:40 +0530

    made changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,23 @@
-import Hero from '@/components/Hero'
-import React from 'react'
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import TopBar from "@/components/TopBar";
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
+      <TopBar />
+      <Header />
+      <Hero />
+      <About />
+      <Stats />
+      <Services />
+      <Testimonial />
+      <Faq />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
